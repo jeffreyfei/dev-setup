63531e8d36c062efce89fb7baf7436f92a6433be
Jeffrey Fei <jeffreyfei@msn.com>
2020-05-25 14:24:59 -0700

    Add plugin installation to vim

--- setup_vim.sh
+++ setup_vim.sh
@@ -7,6 +7,7 @@ source constants.sh
 
 colorscheme="colorscheme jellybeans"
 jellybean_theme_url=https://raw.githubusercontent.com/nanotech/jellybeans.vim/master/colors/jellybeans.vim
+vimcompletesme_url=git://github.com/ajh17/VimCompletesMe.git
 
 
 echo "Update .vimrc? (y/n)"
@@ -21,8 +22,41 @@ if [[ $res = "y" ]]; then
 fi
 
 echo "Install jellybean colorscheme? (y/n)"
+read res
 if [[ $res = "y" ]]; then
     mkdir -p ~/.vim/colors
     cd ~/.vim/colors; curl -O $jellybean_theme_url
     echo $colorscheme >> $vimrc_path
+fi
+
+echo "Install plugins? (y/n)"
+read res
+if [[ $res = "y" ]]; then
+    # Install VimPlug plugins
+    echo "plug#begin('~/.vim/plugged')"  >> $vim_path
+    echo "Install vimtex?"
+    read res
+    if [[ $res = "y" ]]; then
+        echo "Plug 'lervag/vimtex'" >> $vim_path
+    fi
+
+    echo "Install nerdtree?"
+    read res
+    if [[ $res = "y" ]]; then
+        echo "Plug 'preservim/nerdtree'" >> $vim_path
+    fi
+
+    echo "call plug#end()" >> $vim_path
+
+    vim +PlugInstall +qall
+
+    # Insert NerdTree config
+    cat $(pwd)/nerdtree_conf >> $vim_path
+
+    # Install VimCompletesMe
+    echo "Install VimCompletesMe?"
+    read res
+    if [[ $res = "y" ]]; then
+        git clone $vimcompletesme_url ~/.vim/pack/vendor/start/VimCompletesMe
+    fi
 fi
